e1d7833668939b2d096ff0ce5bdfab76e0162b72
Shaun Curtis <shaun@coldelm.com>
2025-01-15 08:52:07 +0000

    Updates

---
+++ Source/Application/Blazr.App.Core/Invoices/DomainObjects/Invoice.cs
@@ -0,0 +1,35 @@
+﻿namespace Blazr.App.Core;
+
+public sealed class Invoice
+{
+    private bool _isDirty;
+    public DmoInvoice InvoiceRecord { get; private set; }
+
+    public DmoCustomer Customer { get; private set; }
+
+    public IEnumerable<InvoiceItem> InvoiceItems 
+        => this.Items.AsEnumerable();
+
+    public bool IsDirty 
+        => _isDirty ? true : this.Items.Any(item => item.IsDirty) ;
+
+    internal List<InvoiceItem> Items { get; private set; } 
+        = new List<InvoiceItem>();
+
+    public Invoice(DmoInvoice invoice, DmoCustomer customer, IEnumerable<DmoInvoiceItem> items)
+    {
+        InvoiceRecord = invoice;
+        Customer = customer;
+
+        foreach (var item in items)
+        {
+            Items.Add(new InvoiceItem(item));
+        }
+    }
+
+    internal void UpdateInvoice(DmoInvoice invoice)
+    {
+        this.InvoiceRecord = invoice;
+        _isDirty = true;
+    }
+}
---
+++ Source/Application/Blazr.App.Core/Invoices/DomainObjects/InvoiceItem.cs
@@ -0,0 +1,22 @@
+﻿/// ============================================================
+/// Author: Shaun Curtis, Cold Elm Coders
+/// License: Use And Donate
+/// If you use it, donate something to a charity somewhere
+/// ============================================================
+namespace Blazr.App.Core;
+public sealed class InvoiceItem
+{
+    public DmoInvoiceItem InvoiceItemRecord { get; private set; }
+    public bool IsDirty { get; private set; }
+    public InvoiceItem(DmoInvoiceItem item)
+    {
+        InvoiceItemRecord = item; 
+    }
+
+    internal void UpdateInvoice(DmoInvoiceItem invoiceItem)
+    {
+        this.InvoiceItemRecord = invoiceItem;
+        this.IsDirty = true;
+    }
+
+}

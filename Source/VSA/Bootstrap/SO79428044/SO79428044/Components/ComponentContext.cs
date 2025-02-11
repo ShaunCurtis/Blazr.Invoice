86a2a89aefa6eb3a225b263c578986cf4dc82c4c
Shaun Curtis <shaun@coldelm.com>
2025-02-11 10:45:12 +0000

    Update

---
+++ Source/VSA/Bootstrap/SO79428044/SO79428044/Components/ComponentContext.cs
@@ -0,0 +1,11 @@
+﻿namespace SO79428044.Components;
+
+public class ComponentContext
+{
+    public event EventHandler? AnEvent;
+
+    public void RaiseEvent(object? sender, EventArgs e)
+    {
+        this.AnEvent?.Invoke(sender, e);
+    }
+}
